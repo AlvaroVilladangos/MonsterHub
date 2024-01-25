ef577328ed8416d6c9e6976f9457d3f2a17fbf34
AlvaroVilladangos <alvaro.villadangos@iesdonana.org>
2024-01-25 18:18:42 +0100

    se añadido js para formularios

--- resources/views/admin/adminArmors.blade.php
+++ resources/views/admin/adminArmors.blade.php
@@ -120,28 +120,41 @@
 
 
                     @isset($armor)
-                        <form id="armorForm" method="post" action="{{ route('armorUpdate', ['id' => $armor->id]) }}"
+                        <form id="armorEditForm" method="post" action="{{ route('armorUpdate', ['id' => $armor->id]) }}"
                             enctype="multipart/form-data">
                             @csrf
                             @method('put')
-                            <img class="img-fluid mb-3" src="" alt="" name="armorImg">
-                            <input type="file" class="form-control mb-3" name="armorImg">
 
-                            <label for="armorName" class="form-label">Nombre de la armadura</label>
-                            <input type="text" class="form-control mb-3" id="armorName" name="armorName">
+                            <div class="mb-3">
+                                <img class="img-fluid mb-3" src="" alt="" name="armorImg">
+                                <input type="file" class="form-control mb-3" name="armorImg">
+                            </div>
 
-                            <label for="armorDef" class="form-label">Defensa</label>
-                            <input type="text" class="form-control mb-3" id="armorDef" name="armorDef">
+                            <div class="mb-3">
+                                <label for="armorName" class="form-label">Nombre de la armadura</label>
+                                <input type="text" class="form-control mb-3" id="armorName" name="armorName">
+                                <span id="errorArmorName" class="text-primary"></span>
+                            </div>
 
-                            <label for="armorInfo" class="form-label">Información</label>
-                            <textarea type="text" class="form-control mb-3" id="armorInfo" name="armorInfo"> </textarea>
+                            <div class="mb-3">
+                                <label for="armorDef" class="form-label">Defensa</label>
+                                <input type="text" class="form-control mb-3" id="armorDef" name="armorDef">
+                                <span id="errorArmorDef" class="text-primary"></span>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="armorInfo" class="form-label">Información</label>
+                                <textarea type="text" class="form-control mb-3" id="armorInfo" name="armorInfo"> </textarea>
+                            </div>
 
 
-                            <select name="armorMonster_id" id="armorMonster_id" required>
-                                @foreach ($monsters as $monster)
-                                    <option value="{{ $monster->id }}"> {{ $monster->name }} </option>
-                                @endforeach
-                            </select>
+                            <div class="mb-3">
+                                <select name="armorMonster_id" id="armorMonster_id" required>
+                                    @foreach ($monsters as $monster)
+                                        <option value="{{ $monster->id }}"> {{ $monster->name }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
 
                             <button type="submit" class="btn btn-dark"> Actualizar</button>
 
@@ -167,23 +180,34 @@
                     <form id="armorCreateForm" method="post" action="{{ route('armorStore') }}"
                         enctype="multipart/form-data">
                         @csrf
-                        <img class="img-fluid mb-3" src="" alt="" name="armorImg">
-                        <input type="file" class="form-control mb-3" name="armorImg" required>
 
-                        <label for="armorName" class="form-label">Nombre del armadura</label>
-                        <input type="text" class="form-control mb-3" id="armorName" name="armorName" required>
+                        <div class="mb-3">
+                            <img class="img-fluid mb-3" src="" alt="" name="armorImg">
+                            <input type="file" class="form-control mb-3" name="armorImg" required>
+                        </div>
 
-                        <label for="armorDef" class="form-label">Defensa</label>
-                        <input type="text" class="form-control mb-3" id="armorDef" name="armorDef" required>
+                        <div class="mb-3">
+                            <label for="armorName" class="form-label">Nombre del armadura</label>
+                            <input type="text" class="form-control mb-3" id="armorName" name="armorName" required>
+                        </div>
 
-                        <label for="armorInfo" class="form-label">Información</label>
-                        <textarea type="text" class="form-control mb-3" id="armorInfo" name="armorInfo"> </textarea>
+                        <div class="mb-3">
+                            <label for="armorDef" class="form-label">Defensa</label>
+                            <input type="text" class="form-control mb-3" id="armorDef" name="armorDef" required>
+                        </div>
 
-                        <select name="armorMonster_id" id="armorMonster_id" required>
-                            @foreach ($monsters as $monster)
-                                <option value="{{ $monster->id }}"> {{ $monster->name }} </option>
-                            @endforeach
-                        </select>
+                        <div class="mb-3">
+                            <label for="armorInfo" class="form-label">Información</label>
+                            <textarea type="text" class="form-control mb-3" id="armorInfo" name="armorInfo"> </textarea>
+                        </div>
+
+                        <div class="mb-3">
+                            <select name="armorMonster_id" id="armorMonster_id" required>
+                                @foreach ($monsters as $monster)
+                                    <option value="{{ $monster->id }}"> {{ $monster->name }} </option>
+                                @endforeach
+                            </select>
+                        </div>
 
 
                         <button type="submit" class="btn btn-dark"> Crear</button>
@@ -204,6 +228,9 @@
 
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 
     <script>
         $(document).ready(function() {
@@ -229,11 +256,6 @@
                         $('#armorForm').attr('action', '/armor/' + armorId + '/update');
                     }
                 });
-                armor / {
-                    id
-                }
-                /update
-
 
                 $('#armorEditModal').modal('show');
             });
@@ -241,8 +263,6 @@
     </script>
 
 
-
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
     <script>
@@ -263,5 +283,114 @@
                 }
             })
         }
+    </script>
+
+
+    <script>
+        $.validator.addMethod("regex", function(value, element, regexpr) {
+            return regexpr.test(value);
+        }, "Por favor, introduce un valor válido.");
+
+        $("#armorCreateForm").validate({
+            rules: {
+                armorImg: {
+                    required: true,
+                    extension: "jpg|png|jpeg"
+                },
+                armorName: {
+                    required: true,
+                    minlength: 2,
+                    regex: /^[A-ZÁÉÍÓÚÑ][a-zA-ZáéíóúÁÉÍÓÚÑñ\s]*$/
+                },
+                armorDef: {
+                    required: true,
+                    number: true
+                },
+                armorInfo: {
+                    required: true,
+                    minlength: 10
+                }
+            },
+            messages: {
+                armorImg: {
+                    required: "Por favor, selecciona una imagen",
+                    extension: "Por favor, selecciona una imagen válida (jpg, png, jpeg)"
+                },
+                armorName: {
+                    required: "Por favor, introduce el nombre de la armadura",
+                    minlength: "El nombre de la armadura debe tener al menos 2 caracteres",
+                    regex: "La primera letra del nombre de la armadura debe ser mayúscula. El nombre puede contener solo letras, incluyendo tildes, la letra 'ñ' y espacios."
+                },
+                armorDef: {
+                    required: "Por favor, introduce la defensa de la armadura",
+                    number: "Por favor, introduce un número válido para la defensa"
+                },
+                armorInfo: {
+                    required: "Por favor, introduce la información de la armadura",
+                    minlength: "La información de la armadura debe tener al menos 10 caracteres"
+                }
+            },
+            submitHandler: function(form) {
+                form.submit();
+            }
+        });
+    </script>
+
+    <script>
+        function validarArmorName(armorName) {
+            var regexCaracteres = /^[A-ZÁÉÍÓÚÑ][a-zA-ZáéíóúÁÉÍÓÚÑñ\s]*$/;
+            var error = document.getElementById("errorArmorName");
+
+            if (!regexCaracteres.test(armorName)) {
+                error.textContent =
+                    "La primera letra del nombre de la armadura debe ser mayúscula. El nombre puede contener solo letras.";
+                document.getElementById("armorName").value = '';
+                return false;
+            }
+            if (armorName.length > 20) {
+                error.textContent = "El nombre de la armadura no puede tener más de 20 caracteres.";
+                document.getElementById("armorName").value = '';
+                return false;
+            }
+            if (armorName.length < 4) {
+                error.textContent = "El nombre de la armadura no puede tener menos de 4 letras.";
+                document.getElementById("armorName").value = '';
+                return false;
+            } else {
+                error.textContent = "";
+                return true;
+            }
+        }
+
+        function validarArmorDef(armorDef) {
+            var error = document.getElementById("errorArmorDef");
+
+            if (isNaN(armorDef) || armorDef < 0 || armorDef > 200) {
+                error.textContent = 'La defensa de la armadura debe ser un número entre 0 y 200';
+                document.getElementById("armorDef").value = '';
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        document.getElementById('armorEditForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            var isValid = true;
+
+            var armorName = document.getElementById('armorName').value;
+            if (!validarArmorName(armorName)) {
+                isValid = false;
+            }
+
+            var armorDef = document.getElementById('armorDef').value;
+            if (!validarArmorDef(armorDef)) {
+                isValid = false;
+            }
+
+            if (isValid) {
+                this.submit();
+            }
+        });
     </script>
 @endsection
